0ef4723dca2a77332a6b5f8c00f09ce17f59a4ad
liyp0095 <liyp0095@163.com>
2016-05-18 15:16:42 +0800

    test code for matlab original HDR function

--- code_test/main.m
+++ code_test/main.m
@@ -3,3 +3,24 @@ addpath('handle');
 
 testfun('testfile.txt', 1, 2);
 testhandle();
+
+%% 
+path = 'file';
+
+mydir = dir(path);
+N = length(mydir);
+filename = {mydir(3:end-1).name};
+
+for i = 1:length(filename)
+    inname = {};
+    list = dir(fullfile(path, filename{i}, '*.jpg'));
+    M = length(list);
+    inname = {list(3:end).name};
+%     for j = 1:M-2
+%         % disp(fullfile(path, filename(i), list(j).name));
+%         inname{j} = fullfile(path, filename(i), list(j).name);
+%     end
+    addpath([path '\' filename{i}]);
+    hdr = makehdr(inname);
+    hdrwrite(hdr, 'one.hdr');
+end 
