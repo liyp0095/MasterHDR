45f6205bd5a2c840b904b612b939ff64e96d42fe
liyp0095 <liyp0095@163.com>
2016-06-24 13:35:38 +0800

    DeepLearnToolbox

--- HDR/codes/kms.m
+++ HDR/codes/kms.m
@@ -1,26 +1,30 @@
 addpath('..\hdr_pics');
 
-hdr = hdrread('Oxford_Church.hdr');
+hdr = hdrread('hdr_image.hdr');
+
+% hdr = min(1,max(0, imresize(hdr,1/16) )); 
+
 hdr = hdr./max(max(max(hdr)));
 
 grayhdr = rgb2gray(hdr);
 imhist(grayhdr);
+figure
 
-X = grayhdr;
+X = imresize(grayhdr,1/16);
 X = X(:);
 % X = randn(1000, 1);
 
 opts = statset('Display','final');
 
-[Idx,Ctrs,SumD,D] = kmeans(X,2,'Replicates',2,'Options', opts);
+[Idx,Ctrs,SumD,D] = kmeans(X,4,'Replicates',4,'Options', opts);
 
 plot(X(Idx==1,1),'r.','MarkerSize',1)
 hold on
 plot(X(Idx==2,1),'b.','MarkerSize',1)
-% hold on
-% plot(X(Idx==3,1),'g.','MarkerSize',1)
-% hold on
-% plot(X(Idx==4,1),'y.','MarkerSize',1)
+hold on
+plot(X(Idx==3,1),'g.','MarkerSize',1)
+hold on
+plot(X(Idx==4,1),'y.','MarkerSize',1)
 % hold on
 % plot(X(Idx==5,1),'m.','MarkerSize',1)
 % hold on
